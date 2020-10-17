ad52317252173e47d039e155d8ab3d6cc79daf00
3so <11akihiro10@gmail.com>
2020-10-17 16:24:04 +0900

    usersテーブルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true do
+    validates :nickname
+    validates :password,            format: { with: PASSWORD_REGEX, message: "Password Include both letters and numbers" }
+    validates :family_name,         format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Last name Full-width characters"}
+    validates :first_name,          format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "First name Full-width characters" }
+    validates :family_name_reading, format: { with: /\A[ァ-ン]+\z/, message: "Last name kana Full-width katakana characters" }
+    validates :first_name_reading,  format: { with: /\A[ァ-ン]+\z/, message: "First name kana Full-width katakana characters" }
+    validates :birthday
+  end
 end
