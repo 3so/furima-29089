5887a4beca1b3759c7568125278eaa9e71b1b329
3so <11akihiro10@gmail.com>
2020-10-25 17:35:25 +0900

    販売利益の表示

--- app/javascript/calc_sales_commission_and_profit.js
+++ app/javascript/calc_sales_commission_and_profit.js
@@ -2,8 +2,11 @@ function calcSalesCommission() {
   const itemPrice = document.getElementById("item-price");
   itemPrice.addEventListener("keyup", () => {
   const calcSalesCommission = Math.floor(itemPrice.value * 0.1);
+  const calcProfit = (itemPrice.value - calcSalesCommission);
   const salesCommission = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
   salesCommission.innerHTML = `${calcSalesCommission}`;
+  profit.innerHTML = `${calcProfit}`
   });
 }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../calc_sales_commission")
+require("../calc_sales_commission_and_profit")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
