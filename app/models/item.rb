2576578aaf7e999a0526e49415e14b55aeb00b73
3so <11akihiro10@gmail.com>
2020-10-25 13:51:55 +0900

    itemsテーブルのバリデーション設定
    itemモデル、itemsテーブル、itemsコントローラーはユーザー登録機能実装時に作成済み

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charges_payer_id
+    validates :shipping_location_id
+    validates :lead_time_id
+    validates :price
+    validates :user_id, foreign_key: true
+  end
 end
