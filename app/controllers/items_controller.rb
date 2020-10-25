7784e2595ecf18d643cb59e54652c3c68bb45ffd
3so <11akihiro10@gmail.com>
2020-10-25 16:20:17 +0900

    itemsテーブルへの製品データ保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_payer_id, :shipping_location_id, :lead_time_id, :price, :user_id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charges_payer_id, :shipping_location_id, :lead_time_id, :price).merge(user_id: current_user.id)
   end
 end
