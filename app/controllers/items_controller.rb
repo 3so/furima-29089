e18eab04a9f0f20f5523b032b317a62fb78322a7
3so <11akihiro10@gmail.com>
2020-11-07 15:13:22 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,9 +24,7 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.id != current_user.id
-      redirect_to item_path(@item.id)
-    end
+    redirect_to item_path(@item.id) if @item.id != current_user.id
   end
 
   def update
@@ -48,5 +46,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-
 end
