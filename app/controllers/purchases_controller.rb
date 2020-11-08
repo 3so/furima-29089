eed814f0929dd8a9db4e3eab635b2f69d2206c5c
3so <11akihiro10@gmail.com>
2020-11-08 15:17:21 +0900

    purchasesコントローラーのcreateアクション編集
    ・payjpのgemを用いて、決済を行えるよう編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -6,9 +6,9 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    binding.pry
     @purchase_shipping_address = PurchaseShippingAddress.new(purchase_shipping_address_params)
     if @purchase_shipping_address.valid?
+      pay_item
       @purchase_shipping_address.save
       redirect_to root_path
     else
@@ -24,5 +24,14 @@ class PurchasesController < ApplicationController
 
   def purchase_shipping_address_params
     params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_shipping_address_params[:token],
+      currency: 'jpy'
+    )
   end
 end
