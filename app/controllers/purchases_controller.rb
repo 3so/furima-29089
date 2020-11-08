4a9b8edcb4d4cd4ff692d156dad31a6f07609191
3so <11akihiro10@gmail.com>
2020-11-08 13:38:46 +0900

    purchaseコントローラーのcreateアクション定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,28 @@
 class PurchasesController < ApplicationController
+  before_action :set_item, only: [:index, :create]
 
   def index
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    binding.pry
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_shipping_address_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render action: :index
+    end
+  end
+
+  private
+
+  def set_item
     @item = Item.find(params[:item_id])
   end
 
+  def purchase_shipping_address_params
+    params.require(:purchase_shipping_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id)
+  end
 end
