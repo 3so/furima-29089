f8fa8883e613e143a389056d17e061a8ab91718a
3so <11akihiro10@gmail.com>
2020-11-08 15:17:41 +0900

    token属性のバリデーション設定

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -4,6 +4,7 @@ class PurchaseShippingAddress
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :token
 
   with_options presence: true do
+    validates :token
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'Input correctly' }
@@ -15,6 +16,6 @@ class PurchaseShippingAddress
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
-    shipping_address = ShippingAddress.new(postal_code: postal_code, prefecture_id: prefecture_id, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
+    shipping_address = ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
